c7eb9d626954a5c05a4137fcd51e616cd98438e9
NhatAnh05 <23130006@st.hcmuaf.edu.vn>
2025-12-02 14:51:09 +0700

    Update TestDao.java

--- main/java/TestDao.java
+++ main/java/TestDao.java
@@ -5,8 +5,22 @@ import Model_Layer.SongDAO;
 
 public class TestDao {
 	public static void main(String[] args) {
-		 SongDAO dao = new SongDAO();
-	        dao.getAllSongs().forEach(song -> System.out.println(song.getTitle()));
-    }
+		 SSongDAO dao = new SongDAO();
+        List<Song> songs = dao.getAllSongs();
 
+        // In tiêu đề bảng
+        System.out.printf("%-5s %-30s %-20s %-15s %-6s%n", "ID", "Title", "Artist", "Genre", "Views");
+        System.out.println("-------------------------------------------------------------------------------");
+
+        // In từng bài hát
+        for (Song song : songs) {
+            System.out.printf("%-5d %-30s %-20s %-15s %-6d%n",
+                    song.getId(),
+                    song.getTitle(),
+                    song.getArtist(),
+                    song.getGenre(),
+                    song.getViews()
+            );
+        }
+	}
 }
