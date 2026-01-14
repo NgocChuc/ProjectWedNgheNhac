0121723aee37b580b602cf47ee6f179d3165aefc
NgocChuc <23130032@st.hcmuaf.edu.vn>
2026-01-14 14:20:10 +0700

    cập nhật Playlist class with new attributes and methods
    Refactor Playlist class to include description and userId attributes.

--- main/java/Model_Layer/Playlist.java
+++ main/java/Model_Layer/Playlist.java
@@ -1,43 +1,59 @@
 package Model_Layer;
 
 public class Playlist {
-	private int id;
-	private String name;
-	private String coverImage;
-	
-	public Playlist() { }
-	
-	public Playlist(int id, String name, String coverImage) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.coverImage = coverImage;
-	}
+    // 1. Các thuộc tính (Phải khớp kiểu dữ liệu với Database)
+    private int id;               
+    private String name;            
+    private String description;    
+    private String coverImage;      
+    private int userId;             
 
-	public int getId() {
-		return id;
-	}
+    public Playlist() {
+    }
+    public Playlist(int id, String name, String description, String coverImage, int userId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.coverImage = coverImage;
+        this.userId = userId;
+    }
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getCoverImage() {
-		return coverImage;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setCoverImage(String coverImage) {
-		this.coverImage = coverImage;
-	}
-	
-	
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
+    public String getCoverImage() {
+        return coverImage;
+    }
+
+    public void setCoverImage(String coverImage) {
+        this.coverImage = coverImage;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
