112723fb5fcdc84bc128ed5c045c61371bbe3828
NhatAnh05 <23130006@st.hcmuaf.edu.vn>
2026-01-16 16:59:37 +0700

    Refactor HomeServlet to handle multiple URL patterns

--- main/java/Controller_Layer/HomeServlet.java
+++ main/java/Controller_Layer/HomeServlet.java
@@ -1,37 +1,39 @@
 package Controller_Layer;
 
-import Model_Layer.*;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest; // Quan trọng
 import javax.servlet.http.HttpServletResponse; // Quan trọng
+
+import Model_Layer.Playlist;
+import Model_Layer.Song;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import Dao.AlbumDAO;
 import Dao.ArtistDAO;
 import Dao.SongDAO;
-import Model_Layer.Song;
 
-@WebServlet("/home")
+@WebServlet(urlPatterns = { "/home", "/index" })
 public class HomeServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=UTF-8");
+		SongDAO songDAO = new SongDAO();
 
-        System.out.println(">>> HomeServlet CALLED <<<");
+	    // 1. Lấy dữ liệu
+	    List<Song> randomList = songDAO.getRandomSongs(6);
 
-        SongDAO dao = new SongDAO();
-        List<Song> randomList = dao.getRandomSongs(6);
+	    // 2. GỬI DỮ LIỆU SANG JSP 
+	    req.setAttribute("listRandom", randomList); 
 
-        System.out.println(">>> Random songs size = " + randomList.size());
-
-        req.setAttribute("songs", randomList);
-
-        req.getRequestDispatcher("/views/home.jsp").forward(req, resp);
-    }
+	    req.getRequestDispatcher("/index.jsp").forward(req, resp);
+	}
 }
-
